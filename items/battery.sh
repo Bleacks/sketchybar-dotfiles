8542be019db97ad4fbc1c9a406e89210dfbe6407
Bleacks <bleacks@gmail.com>
2023-06-07 01:46:17 +0200

    Battery - Improved support and status update

--- items/battery.sh
+++ items/battery.sh
@@ -3,14 +3,26 @@
 battery=(
   script="$PLUGIN_DIR/battery.sh"
   icon.font="$FONT:Regular:19.0"
-  padding_right=5
-  padding_left=0
-  label.drawing=off
+  padding_right=7
+  padding_left=7
+  label=" - "
+  label.drawing=on
+  label.padding_left=3
+  icon="$BATTERY_100"
   update_freq=120
   updates=on
 )
 
+battery_bracket=(
+  background.color="$BATTERY_BACKGROUND"
+  background.border_color="$BATTERY_BORDER_100"
+  background.border_width=2
+  background.padding_right=5
+)
+
 sketchybar --add item battery right      \
            --set battery "${battery[@]}" \
-           --subscribe battery power_source_change system_woke mouse.clicked mouse.entered mouse.exited
+           --subscribe battery power_source_change system_woke mouse.clicked
            
+sketchybar --add bracket battery_bracket battery \
+           --set battery_bracket "${battery_bracket[@]}"
--- plugins/battery.sh
+++ plugins/battery.sh
@@ -8,61 +8,49 @@ PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 REMAINING=$(echo "$BATTERY_INFO" | grep -Eo "[0-9:]+ remaining" | cut -d ' ' -f1)
 
-if [ $PERCENTAGE = "" ]; then
-  sketchybar --animate sin 20 --set $NAME label="nil" label.drawing=on
-  exit 0
-fi
+refresh() {
+  if [ $PERCENTAGE = "" ]; then
+    sketchybar --animate sin 20 --set $NAME label=" - "
+    exit 0
+  fi
 
-DRAWING=on
-COLOR=$WHITE
-case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100
-  ;;
-  [6-8][0-9]) ICON=$BATTERY_75
-  ;;
-  [3-5][0-9]) ICON=$BATTERY_50
-  ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
-  ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
-esac
+  case ${PERCENTAGE} in
+    9[0-9]|100) ICON=$BATTERY_100; COLOR=$BATTERY_BORDER_100
+    ;;
+    [6-8][0-9]) ICON=$BATTERY_75; COLOR=$BATTERY_BORDER_75
+    ;;
+    [3-5][0-9]) ICON=$BATTERY_50; COLOR=$BATTERY_BORDER_50
+    ;;
+    [1-2][0-9]) ICON=$BATTERY_25; COLOR=$BATTERY_BORDER_25
+    ;;
+    *) ICON=$BATTERY_0; COLOR=$BATTERY_BORDER_0
+  esac
 
-if [[ $CHARGING != "" ]]; then
-  ICON=$BATTERY_CHARGING
-fi
+  if [[ $CHARGING != "" ]]; then
+    ICON=$BATTERY_CHARGING
+  fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
-
+  sketchybar  --set $NAME icon="$ICON" \
+              --set battery_bracket background.border_color="$COLOR"
+}
 
 mouse_clicked() {
-  label=$(sketchybar --query $NAME | jq -r ".label.value")
-  if [ "$label" = "" ]; then
-    sketchybar --animate sin 20 --set $NAME label="$PERCENTAGE" label.drawing=on
+  LABEL=$(sketchybar --query $NAME | jq -r ".label.value")
+  if [[ "$LABEL" == *% ]]
+  then
+    sketchybar --animate sin 20 --set $NAME label="$REMAINING"
+  elif [[ "$LABEL" == *:* ]]
+  then
+    sketchybar --animate sin 20 --set $NAME label=""
   else
-    sketchybar --animate sin 20 --set $NAME label="" label.drawing=off
+    sketchybar --animate sin 20 --set $NAME label="${PERCENTAGE}%"
   fi
-}
-
-mouse_entered() {
-  label_value="$REMAINING"
-  if [ "$REMAINING" = "" ]; then
-    label_value="?"
-  fi
-  sketchybar --animate sin 20 --set $NAME label="$label_value"
-}
-
-mouse_exited() {
-  drawing=$(sketchybar --query $NAME | jq -r ".label.drawing")
-  if [ "$drawing" = "on" ]; then
-    sketchybar --animate sin 20 --set $NAME label="$PERCENTAGE"
-  fi
+  refresh
 }
 
 case "$SENDER" in
   "mouse.clicked") mouse_clicked
   ;;
-  "mouse.entered") mouse_entered
-  ;;
-  "mouse.exited") mouse_exited
+  *) refresh
   ;;
 esac
